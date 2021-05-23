ca9bb50ac14187ad6af20acd954a4e8717e86b77
avalonv <avalonvales@protonmail.com>
2021-05-23 18:07:25 -0300

    use Coingecko API

--- crypto.py
+++ crypto.py
@@ -2,32 +2,33 @@
 
 import configparser
 import sys
-import requests
 from decimal import Decimal
+from pycoingecko import CoinGeckoAPI
 
+cg = CoinGeckoAPI()
 config = configparser.ConfigParser()
 
 # File must be opened with utf-8 explicitly
 with open('~/.config/polybar/crypto-config', 'r', encoding='utf-8') as f:
-	config.read_file(f)
+        config.read_file(f)
 
 # Everything except the general section
-currencies = [x for x in config.sections() if x != 'general']
-base_currency = config['general']['base_currency']
+currencies = [x.lower() for x in config.sections() if x != 'general']
+base_currency = config['general']['base_currency'].lower()
 params = {'convert': base_currency}
 
 
 for currency in currencies:
-	icon = config[currency]['icon']
-	json = requests.get(f'https://api.coinmarketcap.com/v1/ticker/{currency}',
-					 	params=params).json()[0]
-	local_price = round(Decimal(json[f'price_{base_currency.lower()}']), 2)
-	change_24 = float(json['percent_change_24h'])
+    icon = config[currency]['icon']
+    coin_dict = cg.get_price(ids=f'{currency}', include_24hr_change='true',
+        vs_currencies=f'{base_currency}')
+    local_price = round(Decimal(coin_dict[f'{currency}'][f'{base_currency}']), 2)
+    change_24 = round(Decimal(coin_dict[f'{currency}']['usd_24h_change']), 1)
 
-	display_opt = config['general']['display']
-	if display_opt == 'both' or display_opt == None:
-		sys.stdout.write(f'{icon} {local_price}/{change_24:+}%  ')
-	elif display_opt == 'percentage':
-		sys.stdout.write(f'{icon} {change_24:+}%  ')
-	elif display_opt == 'price':
-		sys.stdout.write(f'{icon} {local_price}  ')
+    display_opt = config['general']['display']
+    if display_opt == 'both' or display_opt == None:
+        sys.stdout.write(f'{icon} {local_price}/{change_24:+}%  ')
+    elif display_opt == 'percentage':
+        sys.stdout.write(f'{icon} {change_24:+}%  ')
+    elif display_opt == 'price':
+        sys.stdout.write(f'{icon} {local_price}  ')
